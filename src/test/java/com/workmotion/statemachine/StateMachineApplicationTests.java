a240852aba7ed58541a3d1bc08be6fcd0f206194
abdelaziizz <aelazizashraf@gmail.com>
2023-02-07 01:08:57 +0200

    db connection and modifications

--- src/test/java/com/workmotion/statemachine/StateMachineApplicationTests.java
+++ src/test/java/com/workmotion/statemachine/StateMachineApplicationTests.java
@@ -1,13 +1,13 @@
-package com.workmotion.statemachine;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StateMachineApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.workmotion.statemachine;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StateMachineApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
